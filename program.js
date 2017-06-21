9c7e7a384abb61fa78f06bf9c57aecd63d6c6bcf
Johnson, Richard <rjohnson@cdbaby.com>
2017-06-21 13:09:34 -0700

    Completed collect http

--- program.js
+++ program.js
@@ -4,12 +4,18 @@ var fs = require('fs');
 var path = require('path');
 var ext = process.argv[3];
 var http = require('http');
+var bl = require('bl');
+
 
 function callback (res){
     res.setEncoding('utf8');
-    res.on("data", function(data){
-        console.log(data);
-    })
+    res.pipe(bl(function(err, data){
+        console.log(data.length);
+        console.log(data.toString());
+    }))
+    //res.on("data", function(data){
+    //    console.log(data);
+    //})
 }
 
 http.get(process.argv[2], callback);
